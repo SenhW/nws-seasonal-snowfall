38068ed3fd13dccdda917007bb68ad159e5fd3b7
SenhW <senhwang3@gmail.com>
2015-02-10 00:57:50 -0500

    bgm

--- cities.py
+++ cities.py
@@ -1,3 +1,8 @@
 cities = [['Boston,MA', 'Hartford,CT', 'Worcester,MA', 'Providence,RI'],
-          ['New York (Central Park),NY', 'New York (LaGuardia),NY', 'New York (JFK),NY', 'Islip,NY',
-           'Bridgeport,CT', 'Newark,NJ']]
+          ['New York (Central Park),NY', 'New York (LaGuardia),NY', 'New York (JFK),NY', 'Islip,NY', 'Bridgeport,CT',
+           'Newark,NJ'],
+          ['Portland,ME', 'Concord,NH', 'Gray,ME', 'Manchester,NH'],
+          ['Bangor,ME', 'Caribou,ME'],
+          ['Burlington,VT'],
+          ['Albany,NY'],
+          ['Allentown,PA', 'Atlantic City,NJ', 'Philadelphia,PA', 'Wilmington,DE']]
